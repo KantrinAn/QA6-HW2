6aa2d81b85d1ac13ad5beea6560aff8d0abad25b
Katrin <anpipinakaka@gmail.com>
2023-11-02 23:55:40 +0200

    upd hw3 task1

--- src/homework3/Task1.java
+++ src/homework3/Task1.java
@@ -8,11 +8,16 @@ public class Task1 {
  Необходимо при помощи только метода next() класса Scanner (не используем метод nextLine()) присвоить перемнным
  типа String следующие значения: string1 = Я string2 = тестирую string3 = замечательно string4 = Что еще нужно?
        Помимо string1, string2, string3, string4 новых переменных создавать нельзя.*/
+
       Scanner scanner = new Scanner(System.in);
-        System.out.print("start enter text");
+        System.out.print("Я тестирую замечательно. Что еще нужно?");
         String string1 = scanner.next();
         String string2 = scanner.next();
         String string3 = scanner.next();
-        String string4 = scanner.next();
+        String string4 = scanner.next() +" " + scanner.next() + " " + scanner.next();
+        System.out.println(string1);
+        System.out.println(string2);
+        System.out.println(string3);
+        System.out.println(string4);
               }
 }
